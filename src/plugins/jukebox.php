99f0be0b647dce2c36944b95866ebefa96223a21
Pavlo Lavrenenko <santa.ssh@gmail.com>
2015-07-31 21:31:11 +0300

    Rewrite jukebox plugin
    Fetch lyrics via api.musixmatch.com (an account on developer.musixmatch.com
    with valid appkey is required).

--- src/plugins/jukebox.php
+++ src/plugins/jukebox.php
@@ -26,42 +26,78 @@ require_once __DIR__ . '/../plugins.php';
 require_once __DIR__ . '/../globals.php';
 require_once JAXL_DIR . '/jaxl.php';
 
-define('URI_REGEX',   '/<h3 class=\"r\"><a href=\"\/url\?q=(.+)&amp;/U');
-define('SONGS_REGEX', '/<li><a href=\"(.+)\" title=\"(.+) Lyrics\">/U');
-define('SONG_REGEX',  '/<script src=\"\/\/srv\.tonefuse.+\"><\/script><br>(.+)<script>/Us');
-
-define('LYRICS_HOST', 'www.sing365.com');
+# Refer to https://developer.musixmatch.com for details.
+define('API_HOST', 'api.musixmatch.com/ws/1.1');
 define('GOOGLE_HOST', 'www.google.com');
 
-function match_content($uri, $regex, $timeout = 3, $all = false) {
+$cache_artists = array();
+$cache_albums = array();
+$cache_tracks = array();
+$cache_lyrics = array();
+
+function call_api($method, $params, $timeout) {
     $context = stream_context_create(
         array('http' => array('timeout' => $timeout)));
+
+    $uri_params = array();
+    foreach ($params as $name => $value) {
+        array_push($uri_params, $name . '=' . $value);
+    }
+
+    $uri_params = implode('&', $uri_params);
+    $uri = 'http://' . API_HOST . '/' . $method . '?' . $uri_params;
+
+    $ts = microtime(true);
     if (($result = @file_get_contents($uri, false, $context)) === false) {
-        _warning('HTTP request failed:'
-            . ' uri = "' . $uri
-            . '", timeout = ' . $timeout);
+        _warning('Request timed out: ' . $uri);
         return null;
     }
 
-    if ($all) {
-        $match = preg_match_all($regex, $result, $match_result);
-    } else {
-        $match = preg_match($regex, $result, $match_result);
+    $request_time = round(microtime(true) - $ts, 3);
+    _info('Request finished in ' . $request_time . ' seconds: ' . $uri);
+
+    $response = json_decode($result);
+    return $response->{'message'}->{'body'};
+}
+
+function trim_text($text, $max_paragraphs) {
+    $trimmed_text = '';
+    $paragraphs = 0;
+
+    foreach (explode("\n", $text) as $line) {
+        $line = trim($line);
+        $trimmed_text .= $line . "\n";
+
+        if ($line == '') {
+            $paragraphs++;
+        }
+        if ($paragraphs == $max_paragraphs) {
+            break;
+        }
     }
 
-    if ($match === false) {
-        _warning('Regex match failed:'
-            . ' uri = "' . $uri
-            . '", regex = "' . $regex
-            . '", all = ' . $all);
-        return null;
-    } else {
-        return $match_result;
+    return $trimmed_text;
+}
+
+function search_link($artist, $track) {
+    $search_artist = '';
+    foreach (explode(' ', $artist) as $token) {
+        $search_artist .= $token . '+';
     }
+
+    $search_track = '';
+    foreach (explode(' ', $track) as $token) {
+        $search_track .= '+' . $token;
+    }
+
+    return 'http://' . GOOGLE_HOST . '/search?q='
+        . htmlspecialchars($search_artist) . '-'
+        . htmlspecialchars($search_track);
 }
 
 function jukebox_handler($argument) {
     global $config;
+    global $cache_artists, $cache_albums, $cache_tracks, $cache_lyrics;
 
     if (isset($argument)) {
         return null;
@@ -69,82 +105,111 @@ function jukebox_handler($argument) {
 
     $artists = $config['jukebox']['artists'];
     $timeout = $config['jukebox']['timeout'];
+    $apikey = $config['jukebox']['apikey'];
 
-    $artist = $artists[mt_rand(0, count($artists) - 1)];
-    $search_artist = '';
-    foreach (explode(' ', $artist) as $token) {
-        $search_artist .= $token . '+';
+    $artist_name = $artists[mt_rand(0, count($artists) - 1)];
+
+    # Request an artist id.
+
+    if (!array_key_exists($artist_name, $cache_artists)) {
+        $response = call_api('artist.search', array(
+            'apikey'    => $apikey,
+            'q_artist'  => urlencode($artist_name),
+            'page_size' => 1
+        ), $timeout);
+
+        $artist = $response->{'artist_list'}[0]->{'artist'};
+        $cache_artists[$artist_name] = array(
+            $artist->{'artist_id'},
+            $artist->{'artist_name'}
+        );
     }
 
-    _info('Looking for artist ' . $artist);
-    $search_uri = 'http://' . GOOGLE_HOST . '/search?q='
-        . htmlspecialchars('site:' . LYRICS_HOST . '+'
-        . $search_artist . 'lyrics&num=1');
-    $artist_match = match_content($search_uri, URI_REGEX, $timeout);
-    if ($artist_match == null) {
-        return null;
-    }
+    $artist = $cache_artists[$artist_name];
+    $artist_id = $artist[0];
+    $artist_name = $artist[1];
 
-    $songs_match = match_content(
-        $artist_match[1], SONGS_REGEX, $timeout, true);
-    if ($songs_match == null) {
-        return null;
-    }
+    # Fetch a list of albums.
 
-    _info('Found ' . count($songs_match[1]) . ' songs');
-    $song_index = mt_rand(0, count($songs_match[1]) - 1);
-    $song_name = $songs_match[2][$song_index];
+    if (!array_key_exists($artist_id, $cache_albums)) {
+        $response = call_api('artist.albums.get', array(
+            'apikey'    => $apikey,
+            'artist_id' => $artist_id,
+            'page_size' => 100
+        ), $timeout);
 
-    _info('Looking for ' . $song_name . ' lyrics');
-    $song_uri = 'http://' . LYRICS_HOST . $songs_match[1][$song_index];
-    $song_match = match_content($song_uri, SONG_REGEX, $timeout);
-    if ($song_match == null) {
-        return null;
-    }
-
-    $max_lines = $config['jukebox']['max_lines'];
-    $max_paragraphs = $config['jukebox']['max_paragraphs'];
-    $lines = 0;
-    $paragraphs = 0;
-
-    $song = strtr($song_match[1], array('<br>' => '' , '<BR>' => ''));
-    $song = preg_replace('/<!-.*->/Us', '', $song);
-    $song_cut = '';
-
-    foreach (explode("\n", $song) as $line) {
-        $line = trim($line);
-        if ($line == '' && $lines == 0) {
-            continue;  // Skip empty lines at the beginning
+        $albums = array();
+        foreach ($response->{'album_list'} as $album) {
+            array_push($albums, array(
+                $album->{'album'}->{'album_id'},
+                $album->{'album'}->{'album_name'}
+            ));
         }
 
-        if ($line == '') {
-            $paragraphs++;
-        } else {
-            $song_cut .= $line;
-            $lines++;
-        }
-
-        if (($paragraphs == 0 && $lines == $max_lines)
-            || $paragraphs == $max_paragraphs
-        ) {
-            break;
-        }
-
-        $song_cut .= "\n";
+        $cache_albums[$artist_id] = $albums;
     }
+
+    $album = $cache_albums[$artist_id];
+    _info('Found ' . count($albums) . ' tracks for artist ' . $artist_name);
+
+    $album = $albums[mt_rand(0, count($albums) - 1)];
+    $album_id = $album[0];
+    $album_name = $album[1];
+
+    # Fetch a list of tracks.
+
+    if (!array_key_exists($album_id, $cache_tracks)) {
+        $response = call_api('album.tracks.get', array(
+            'apikey'       => $apikey,
+            'album_id'     => $album_id,
+            'f_has_lyrics' => true,
+            'page_size'    => 50
+        ), $timeout);
+
+        $tracks = array();
+        foreach ($response->{'track_list'} as $track) {
+            array_push($tracks, array(
+                $track->{'track'}->{'track_id'},
+                $track->{'track'}->{'track_name'}
+            ));
+        }
+
+        $cache_tracks[$album_id] = $tracks;
+    }
+
+    $tracks = $cache_tracks[$album_id];
+    _info('Found ' . count($tracks) . ' tracks in album ' . $album_name);
+
+    $track = $tracks[mt_rand(0, count($tracks) - 1)];
+    $track_id = $track[0];
+    $track_name = $track[1];
+
+    # Fetch lyrics.
+
+    if (!array_key_exists($track_id, $cache_lyrics)) {
+        $response = call_api('track.lyrics.get', array(
+            'apikey'   => $apikey,
+            'track_id' => $track_id
+        ), $timeout);
+
+        $lyrics = $response->{'lyrics'}->{'lyrics_body'};
+        $lyrics = substr($lyrics, 0, strpos($lyrics, '...'));
+
+        $cache_lyrics[$track_id] = $lyrics;
+    }
+
+    # Compose output.
+
+    _info('Baking lyrics for ' . $track_name);
+    $lyrics = trim_text(
+        $cache_lyrics[$track_id],
+        $config['jukebox']['max_paragraphs']);
 
     $output = htmlspecialchars_decode(
-        $artist . ' - ' . $song_name . "\n\n" . $song_cut);
+        $artist_name . ' - ' . $track_name . "\n\n" . $lyrics);
 
     if ($config['jukebox']['google_link']) {
-        $search_song = '';
-        foreach (explode(' ', $song_name) as $token) {
-            $search_song .= '+' . $token;
-        }
-
-        $output .= "\n" . 'http://' . GOOGLE_HOST . '/search?q='
-            . htmlspecialchars($search_artist) . '-'
-            . htmlspecialchars($search_song);
+        $output .= search_link($artist_name, $track_name);
     }
 
     return $output;
